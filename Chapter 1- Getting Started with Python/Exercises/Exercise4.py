bd1063303ff81fac641f287f9c75902a0342af0a
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-10-10 21:35:45 +0400

    Complete

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+# Define three strings in different variables
+string1 = "Hello "
+string2 = "From "
+string3 = "Suriya!"
+
+# Call the function with the strings as arguments 
+output = string1 + string2 + string3 
+print(output)
---
+++ Chapter 1- Getting Started with Python/Exercises/Untitled-1.py
@@ -0,0 +1,32 @@
+print ("Name , Suriya " , 20231)
+print ("Father name" , ": Zahidul Hoque" , 1982 )
+print ("University" ,  "Bathspa" , 0000)
+
+#Variable Reassigment 
+Name  =  "Suriya"
+Name = "Bathspa"
+print (Name)
+
+#Data types int, float, str 
+
+#By default / sign floating numbers // gives integer value 
+print (13/2)
+print (13//2)
+
+name = input ("Enter Name: ")
+print(name)
+
+age = input ("Enter your age: ")
+print (age) 
+Ans = ((2*3/8+(23+5)))
+print (int (Ans)) 
+ 
+#String Concatination  with , and + 
+
+print (" My" , " Name" , "is" , "Suriya")
+
+print ("My " + "name " + "is" "+"  "Suriya") #space is necessary
+
+
+
+
