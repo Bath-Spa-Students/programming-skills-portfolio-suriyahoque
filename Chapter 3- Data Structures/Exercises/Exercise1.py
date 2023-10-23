c5f34feb0991f3b0565f19ef6e26a41d24739f23
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-10-23 12:25:46 +0400

    Create Exercise1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+# Exercise 1: Names :ballot_box_with_check:
+
+# Create a list of friends' names
+names = ["Sumaia", "Sadia", "Aisha", "Urooj", "Salma"]
+
+# Print each person's name by accessing each element in the list
+for name in names:
+    print(name)
