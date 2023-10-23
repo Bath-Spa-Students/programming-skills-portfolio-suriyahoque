74fda1b40fb97e2833a07ea3c3cefa1a2ccfbb08
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-10-23 12:36:45 +0400

    Create Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,9 @@
+# Exercise 3: Your Own List :ballot_box_with_check:
+
+# Create a list of car models
+cars = ["Honda Civic", "Toyota Corolla", "Ford Mustang", "Tesla Model 3", "Chevrolet Camaro"]
+
+# Print statements about these cars
+for car in cars:
+    print(f"I would like to own a {car}.")
+
