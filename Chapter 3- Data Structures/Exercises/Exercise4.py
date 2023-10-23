012595c47251ae4869ef8baabae9176256debe76
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-10-23 12:39:22 +0400

    Create Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+# Exercise 4: Guest List :ballot_box_with_check:
+
+# Create a list of people to invite to dinner
+guests = ["Sumaia", "Aisha", "Urooj"]
+
+# Print invitations to each person
+for guest in guests:
+    print(f"Dear {guest}, I would like to invite you to dinner. Please join us for a wonderful evening!")
