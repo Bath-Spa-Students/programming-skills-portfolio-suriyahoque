3ff38c60f98cb409b84728d0d80c3287c04e98b0
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-10-23 11:51:47 +0400

    Create Exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -0,0 +1,11 @@
+# Assign a message to a variable
+my_message = "I love to play Basketball"
+
+# Print the initial message 
+print(my_message)
+
+# Change the value of the variable to a new message
+my_message = "Playing basketball makes me happy"
+
+# Print the new message
+print(my_message)
