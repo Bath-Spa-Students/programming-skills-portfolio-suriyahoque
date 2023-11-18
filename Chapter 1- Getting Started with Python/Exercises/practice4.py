3a39c38ac9b49c7b1165458140b27c5d5d97f3e1
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-11-18 16:47:03 +0400

    practiceCh1

---
+++ Chapter 1- Getting Started with Python/Exercises/practice4.py
@@ -0,0 +1,6 @@
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+area= (base + height) / 2
+
+print(f"{area}")
---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,9 @@
+# Four strings in different variables
+string1 = "Hello"
+string2 = ", "
+string3 = "world"
+string4 = "!"
+
+# Concatenate the strings and print the result
+result = string1 + string2 + string3 + string4
+print(result)
