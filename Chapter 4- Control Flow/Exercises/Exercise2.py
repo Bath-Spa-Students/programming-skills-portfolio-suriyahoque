c6f1dc614cd7f43f2dc9b653604c7a7c05d92531
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-11-18 09:56:02 +0400

    Create Exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,21 @@
+# Version where the if block runs
+alien_color = 'green'
+
+# Check if the alien's color is green
+if alien_color == 'green':
+    # If true, print a message about earning 5 points
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+else:
+    # If false, print a message about earning 10 points
+    print("Nice shot! You just earned 10 points.")
+
+# Version where the else block runs
+alien_color = 'red'
+
+# Check if the alien's color is green
+if alien_color == 'green':
+    # This block won't be executed if the condition is true
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+else:
+    # If false, print a message about earning 10 points
+    print("Nice shot! You just earned 10 points.")
