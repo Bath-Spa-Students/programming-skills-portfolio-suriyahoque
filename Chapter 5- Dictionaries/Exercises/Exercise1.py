32510dbd36a8a383b169066d65614fc32b941f63
suriyahoque <144208749+suriyahoque@users.noreply.github.com>
2023-11-18 10:06:55 +0400

    Create Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,14 @@
+# Create a dictionary to store information about a person
+person_info = {
+    'first_name': 'Suriya',
+    'last_name': 'Hoque',
+    'age': 18,
+    'city': 'Dhaka'
+}
+
+# Print each piece of information stored in the dictionary
+print("First Name:", person_info['first_name'])
+print("Last Name:", person_info['last_name'])
+print("Age:", person_info['age'])
+print("City:", person_info['city'])
+
